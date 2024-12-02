18c33e87baf3160dbb996301bdabb02889dfa957
Dereelcoder1 <dereelcoder@gmail.com>
2024-12-02 05:29:16 -0800

    starting the Project

---
+++ my-app/src/Pages/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div className='flex bg-blue-500 '>
+        
+    </div>
+  )
+}
+
+export default Home
---
+++ my-app/src/assets/assets.js
@@ -0,0 +1,25 @@
+import home from './home.png'
+import exp from './compass.png'
+import bell from './bell.png'
+import search from './search-interface-symbol'
+import dots from './menu (1).png'
+import dash from './menu.png'
+import user from './user.png'
+import reel from './clapper.png'
+import chat from './chat.png'
+import add from './add.png'
+import user from './user'
+
+export const assets = {
+    home,
+    exp,
+    bell,
+    search,
+    dash,
+    dots,
+    user,
+    reel,
+    chat,
+    add,
+    user
+}
